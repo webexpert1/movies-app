d061d3fab479151fef9180cb1d7c0fb30f4a050e
webexpert1 <richokoye1@gmail.com>
2020-10-28 06:54:10 +0100

    add readme

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import Row from "./components/Row";
 import requests from "./js/requests";
 import Banner from "./components/Banner";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Banner />
       <Row
         title="Netflix Originals"
